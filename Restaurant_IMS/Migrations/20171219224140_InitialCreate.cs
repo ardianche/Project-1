4e1fab46b44d4ae41350d1d648923c2fa5ec78c9
Ardian Ibrahimi <ardianibrahimi057@gmail.com>
2017-12-20 00:17:19 +0100

    Database Migration and Update. A taste of Razor's RenderBody()

---
+++ Restaurant_IMS/Migrations/20171219224140_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Restaurant_IMS.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Kuzhina = table.Column<int>(nullable: false),
+                    Property = table.Column<int>(nullable: false),
+                    emri = table.Column<string>(maxLength: 80, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.Id);
+                });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+        }
+    }
+}
--- Restaurant_IMS/Services/SqlRestaurantData.cs
+++ Restaurant_IMS/Services/SqlRestaurantData.cs
@@ -17,16 +17,20 @@ namespace Restaurant_IMS.Services
         }
         public Restaurant Add(Restaurant restaurant)
         {
-            
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
         }
 
         public Restaurant Get(int id)
         {
-            
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
         }
 
         public IEnumerable<Restaurant> GetAll()
-        { 
+
+        {
+            return _context.Restaurants.OrderBy(r => r.emri);
         }
     }
 }
